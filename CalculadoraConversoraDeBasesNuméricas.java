39134799fa48ec4dc8b8b7f6c180d13dcf081b79
André Luiz <andreluizgbi@gmail.com>
2021-08-26 01:54:35 -0300

    Editando o projeto

--- CalculadoraConversoraDeBasesNuméricas.java
+++ CalculadoraConversoraDeBasesNuméricas.java
@@ -3,6 +3,7 @@ package calculadora.conversora.de.bases.numéricas;
 public class CalculadoraConversoraDeBasesNuméricas {
 
     public static void main(String[] args) {
+	// editando o arquivo
 
     }
     
